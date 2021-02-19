89a045f7022b0ec7c47526507438fc45fdce0b56
Ricardo Nacif <nacif.ricardo@gmail.com>
2021-02-19 16:47:26 -0300

    still nothing

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const adyenEncrypt = (versionParam) => {
-    const version = getVersion(~~versionParam);
-    return require("./lib/0_1_" + version);
-};
 
-const availableVersions = [18, 22, 23, 24, 25];
-
-const getVersion = (version) => {
-    return availableVersions.includes(version) ? version : 24;
-};
-
-module.exports = adyenEncrypt;
+module.exports = require("./lib/0_1_18");
