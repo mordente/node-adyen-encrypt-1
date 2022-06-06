249aaf9bcca317a20321f10cfeb115863a65fc25
mordente <revolutionms3@hotmail.com>
2022-06-06 16:06:21 -0300

    add support to version 21

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const lib = require("./lib/0_1_18");
+const v18 = require("./lib/0_1_18");
+const v21 = require("./lib/0_1_21");
 
-module.exports = function opa() {
-  return lib;
-}
-
-module.exports()
+module.exports = {
+    v18: function () {
+        return v18;
+    },
+    v21: function () {
+        return v21;
+    },
+};
